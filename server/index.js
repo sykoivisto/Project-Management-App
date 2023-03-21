2c426b1b5ce0edf62d7e7b33401bec202937eeff
sykoivisto <sykoivisto@gmail.com>
2023-03-21 12:50:14 -0400

    project and client queries

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// connect to db
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development", // only use in development
+  })
+);
+
+app.listen(port, console.log(`server running on port ${port}`));
